8cc64c1313594836340faaa7f11384e6078589a5
Colin McDonald <colin@bitformations.ca>
2019-01-02 15:54:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ $("#typed_text").typed({
   "A proud Torontonian.^1000", 
   "Always interested in \nnew challenges.^1000", 
   "A full-stack web \napplication specialist.^1000", 
-  "28 years old.^1000", 
+  "31 years old.^1000", 
   "A pretty good sys-admin.^1000", 
   "Not a great cook.^1000", 
   "A morning person.^1000", 
